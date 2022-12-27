dc9c3bd5d018ce0abde17c7bcc177bdeb07024e0
Val-aji <snouval4@gmail.com>
2022-12-27 22:32:26 +0700

    get api

--- src/components/HasilPencarian/headerHasil/index.js
+++ src/components/HasilPencarian/headerHasil/index.js
@@ -1,39 +1,35 @@
 import "./index.css"
+import { useState, useEffect } from "react"
+import axios from "axios"
 
 export default function HeaderHasil() {
+    const [data, setData] = useState([])
+    
+    useEffect(() => {
+        axios.get("https://bootcamp-rent-cars.herokuapp.com/admin/v2/car", {
+            "headers": {
+                "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGJjci5pbyIsInJvbGUiOiJBZG1pbiIsImlhdCI6MTY2NTI0MjUwOX0.ZTx8L1MqJ4Az8KzoeYU2S614EQPnqk6Owv03PUSnkzc"
+            }}).then(res => {
+                setData(res.data.cars)
+                console.log(res.data.cars)
+            })
+    }, [])
     return (
         <div id="headerHasil">
         <section className="hasil-pencarian" id="hasil-pencarian">
                 <div className="container">
-                    <div className="card me-5">
-                        <img src={require("./mobil6.jpg")} className="card-img-top" alt="Kemeja Adj" width={100}/>
+                    {data.length > 0 ? (data.map(item => (
+                        <div key={item.id} className="card me-5">
+                        <img src={item.image} className="card-img-top" alt="Kemeja Adj" width={100}/>
                         <div className="card-body">
-                            <h5 className="card-title">HotWheels</h5>
-                            <p className="card-text">Rp 500.000 / hari</p>
+                            <h5 className="card-title">{item.name}</h5>
+                            <p className="card-text">{item.price}/ hari</p>
                             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
                             <button><a href="#" className="btn">Pilih Mobil</a></button>
                         </div>
                     </div>
+                    ))) : <p>Data Kosong</p>}
                     
-                    <div className="card me-5">
-                        <img src={require("./mobil5.jpg")} className="card-img-top" alt="Kemeja Adj"width={100}/>
-                        <div className="card-body">
-                            <h5 className="card-title">HotWheels</h5>
-                            <p className="card-text">Rp 500.000 / hari</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                            <button><a href="#" className="btn">Pilih Mobil</a></button>
-                        </div>
-                    </div>
-                    
-                    <div className="card">
-                        <img src={require("./mobil4.jpg")} className="card-img-top" alt="Kemeja Putih Hitam Garis" />
-                        <div className="card-body">
-                            <h5 className="card-title">HotWheels</h5>
-                            <p className="card-text">Rp 500.000 / hari</p>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-                            <button><a href="#" className="btn">Pilih Mobil</a></button>
-                        </div>
-                    </div>
                 </div>
                 
             </section>
